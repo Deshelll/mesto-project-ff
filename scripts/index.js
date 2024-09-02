e12083258e912476bad84ae2c6a6e565e6e115a5
Александр Коблов <a.coblov@yandex.ru>
2024-09-02 06:36:17 +0400

    Added the ability to create cards and delete them

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const container = document.querySelector('.content');
 
 // @todo: DOM узлы
+const cardContainer = container.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard (cardTitle, cardLink) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardElement.querySelector('.card__title').textContent = cardTitle;
+    cardElement.querySelector('.card__image').src = cardLink;
+
+    cardDeleteButton.addEventListener('click', deleteCard);
+
+    cardContainer.append(cardElement);
+}
 
 // @todo: Функция удаления карточки
+function deleteCard (event) {
+    const card = event.target.closest('.card');
+    
+    card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach( element => addCard (element.name, element.link));
